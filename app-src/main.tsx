76d141ea332c047947a768180a1517acd0edca1e
Cédric Eberhardt <hello+code@cedeber.fr>
2021-09-25 13:14:34 +0200

    Configure base

--- app-src/main.tsx
+++ app-src/main.tsx
@@ -14,7 +14,7 @@ window.__extern__ = extern;
 
 const App: React.FC = () => {
     return (
-        <Router basename={(import.meta.env.PUBLIC_URL as string) ?? "/"}>
+        <Router basename={(process.env.PUBLIC_URL as string) ?? "/"}>
             <Menu />
             <Switch>
                 <Route path="/worker">
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import react from "@vitejs/plugin-react";
 
+console.log(process.env.PUBLIC_URL);
+
 export default defineConfig({
     plugins: [reactRefresh()],
-    base: "./",
+    base: `${process.env.PUBLIC_URL ?? ""}/`,
     css: { modules: { localsConvention: "camelCase" } },
 });
