7daeb2f9d892eb3e30d6048a71455f1c6dabe2ce
Cédric Eberhardt <hello@cedeber.fr>
2022-02-15 12:29:04 +0100

    some comments

--- .proxyrc.js
+++ .proxyrc.js
@@ -1,8 +1,10 @@
 module.exports = function (app) {
 	app.use((_req, res, next) => {
+		// These headers are mandatory to activate Shared Array Buffer, WebAssembly Threads
+		// because it was deactivated after the Spectre/Meltdown issue discovery.
 		res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
 		res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-		res.setHeader("Cross-Origin-Resource-Policy", "same-origin");
+		// res.setHeader("Cross-Origin-Resource-Policy", "same-origin");
 		next();
 	});
 };
