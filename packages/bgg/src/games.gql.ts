8ce296491504484f131266b5c2d838b122526ada
Cédric Eberhardt <hello+code@cedeber.fr>
2022-08-24 23:14:09 +0200

    update

--- packages/bgg/src/games.gql.ts
+++ packages/bgg/src/games.gql.ts
@@ -1,32 +1,29 @@
-import type * as Types from "../../../graphql";
+import * as Types from '../../../graphql.js';
 
-import { gql } from "@apollo/client";
-import * as Apollo from "@apollo/client";
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 const defaultOptions = {} as const;
 export type GamesQueryVariables = Types.Exact<{
-	username?: Types.InputMaybe<Types.Scalars["String"]>;
+  username?: Types.InputMaybe<Types.Scalars['String']>;
 }>;
 
-export type GamesQuery = {
-	__typename?: "Query";
-	games: Array<{ __typename?: "BoardGame"; id: number; name: string }>;
-};
 
-export type UsersQueryVariables = Types.Exact<{ [key: string]: never }>;
+export type GamesQuery = { __typename?: 'Query', games: Array<{ __typename?: 'BoardGame', id: number, name: string }> };
 
-export type UsersQuery = {
-	__typename?: "Query";
-	users: Array<{ __typename?: "User"; username: string }>;
-};
+export type UsersQueryVariables = Types.Exact<{ [key: string]: never; }>;
+
+
+export type UsersQuery = { __typename?: 'Query', users: Array<{ __typename?: 'User', username: string }> };
+
 
 export const GamesDocument = gql`
-	query Games($username: String) {
-		games(username: $username) {
-			id
-			name
-		}
-	}
-`;
+    query Games($username: String) {
+  games(username: $username) {
+    id
+    name
+  }
+}
+    `;
 
 /**
  * __useGamesQuery__
@@ -44,28 +41,24 @@ export const GamesDocument = gql`
  *   },
  * });
  */
-export function useGamesQuery(
-	baseOptions?: Apollo.QueryHookOptions<GamesQuery, GamesQueryVariables>,
-) {
-	const options = { ...defaultOptions, ...baseOptions };
-	return Apollo.useQuery<GamesQuery, GamesQueryVariables>(GamesDocument, options);
-}
-export function useGamesLazyQuery(
-	baseOptions?: Apollo.LazyQueryHookOptions<GamesQuery, GamesQueryVariables>,
-) {
-	const options = { ...defaultOptions, ...baseOptions };
-	return Apollo.useLazyQuery<GamesQuery, GamesQueryVariables>(GamesDocument, options);
-}
+export function useGamesQuery(baseOptions?: Apollo.QueryHookOptions<GamesQuery, GamesQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GamesQuery, GamesQueryVariables>(GamesDocument, options);
+      }
+export function useGamesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GamesQuery, GamesQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GamesQuery, GamesQueryVariables>(GamesDocument, options);
+        }
 export type GamesQueryHookResult = ReturnType<typeof useGamesQuery>;
 export type GamesLazyQueryHookResult = ReturnType<typeof useGamesLazyQuery>;
 export type GamesQueryResult = Apollo.QueryResult<GamesQuery, GamesQueryVariables>;
 export const UsersDocument = gql`
-	query Users {
-		users {
-			username
-		}
-	}
-`;
+    query Users {
+  users {
+    username
+  }
+}
+    `;
 
 /**
  * __useUsersQuery__
@@ -82,28 +75,25 @@ export const UsersDocument = gql`
  *   },
  * });
  */
-export function useUsersQuery(
-	baseOptions?: Apollo.QueryHookOptions<UsersQuery, UsersQueryVariables>,
-) {
-	const options = { ...defaultOptions, ...baseOptions };
-	return Apollo.useQuery<UsersQuery, UsersQueryVariables>(UsersDocument, options);
-}
-export function useUsersLazyQuery(
-	baseOptions?: Apollo.LazyQueryHookOptions<UsersQuery, UsersQueryVariables>,
-) {
-	const options = { ...defaultOptions, ...baseOptions };
-	return Apollo.useLazyQuery<UsersQuery, UsersQueryVariables>(UsersDocument, options);
-}
+export function useUsersQuery(baseOptions?: Apollo.QueryHookOptions<UsersQuery, UsersQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<UsersQuery, UsersQueryVariables>(UsersDocument, options);
+      }
+export function useUsersLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<UsersQuery, UsersQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<UsersQuery, UsersQueryVariables>(UsersDocument, options);
+        }
 export type UsersQueryHookResult = ReturnType<typeof useUsersQuery>;
 export type UsersLazyQueryHookResult = ReturnType<typeof useUsersLazyQuery>;
 export type UsersQueryResult = Apollo.QueryResult<UsersQuery, UsersQueryVariables>;
 
-export interface PossibleTypesResultData {
-	possibleTypes: {
-		[key: string]: string[];
-	};
-}
-const result: PossibleTypesResultData = {
-	possibleTypes: {},
+      export interface PossibleTypesResultData {
+        possibleTypes: {
+          [key: string]: string[]
+        }
+      }
+      const result: PossibleTypesResultData = {
+  "possibleTypes": {}
 };
-export default result;
+      export default result;
+    
