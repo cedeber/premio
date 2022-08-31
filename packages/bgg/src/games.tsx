42ed2695108d9ceebbe3d8b1bdcf322c74b6bcec
Cédric Eberhardt <hello+code@cedeber.fr>
2022-08-31 07:12:37 +0200

    little utility for graphql

--- packages/bgg/src/games.tsx
+++ packages/bgg/src/games.tsx
@@ -1,20 +1,40 @@
 import { GraphQLClient } from "graphql-request";
 import { GamesQuery, getSdk } from "./games.gql.js";
-import { createSignal } from "solid-js";
+import { Accessor, createSignal } from "solid-js";
 import { useParams } from "@solidjs/router";
 
 const client = new GraphQLClient("http://localhost:4000/graphql", { headers: {} });
 const sdk = getSdk(client);
 
+const loadGraphql = <T extends object>(
+	query: Promise<T>,
+): {
+	loading: Accessor<boolean>;
+	data: Accessor<T | undefined>;
+	error: Accessor<any>;
+} => {
+	const [loading, setLoading] = createSignal<boolean>(true);
+	const [data, setData] = createSignal<T>();
+	const [error, setError] = createSignal<any>();
+
+	query
+		.then(setData)
+		.catch(setError)
+		.then(() => setLoading(false));
+
+	return { loading, data, error };
+};
+
 const Games = () => {
 	const { username } = useParams<{ username: string }>();
-	const [data, setData] = createSignal<GamesQuery>();
 
-	sdk.Games({ username }).then(setData);
+	const { loading, data, error } = loadGraphql(sdk.Games({ username }));
 
 	return (
 		<main>
 			<h1 class="text-3xl font-bold underline">Games for {username}</h1>
+			{loading() && <div>Loading...</div>}
+			{error() && <div>{error}</div>}
 			<div>
 				{data()?.games?.map((game) => (
 					<p>{game.name}</p>
