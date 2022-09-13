53ecccfb75cce1a19d3232ed48d27dd9799fab22
Cédric Eberhardt <hello+code@cedeber.fr>
2022-09-13 23:08:01 +0200

    add eslint-plugin-solid

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
 		"plugin:@typescript-eslint/recommended",
 		"plugin:@typescript-eslint/recommended-requiring-type-checking",
 		"plugin:jsx-a11y/recommended",
+		"plugin:solid/recommended",
 	],
 	parser: "@typescript-eslint/parser",
 	parserOptions: {
@@ -18,7 +19,7 @@ module.exports = {
 		project: "./tsconfig.json",
 		extraFileExtensions: [".jpg", ".png", ".svg", ".css", ".scss", ".woff", ".woff2"],
 	},
-	plugins: ["@typescript-eslint", "jsx-a11y"],
+	plugins: ["@typescript-eslint", "jsx-a11y", "solid"],
 	rules: {
 		"@typescript-eslint/no-unused-vars": [
 			"warn",
