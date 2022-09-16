81f78404de19662547bd9b9e0735b928b7287dce
Cédric Eberhardt <hello+code@cedeber.fr>
2022-09-16 13:43:17 +0200

    Better styling for inputs

--- packages/main/src/pages/Components.tsx
+++ packages/main/src/pages/Components.tsx
@@ -14,6 +14,7 @@ import {
 	HeaderBar,
 	HeaderDivider,
 	ProgressCircle,
+	SimpleTextField,
 	Tag,
 	TagIntent,
 	TextField,
@@ -426,29 +427,80 @@ const Components = () => {
 					</Callout>
 				</div>
 			</Card>
-			<Card title={"Text Field"} icon={"title"} subtitle={"Form"}>
-				<div class={style.vertical} style={{ margin: "10px auto", "max-width": "380px" }}>
-					<TextField label={"Label"} />
-					<TextField label={"Label"} placeholder={"Placeholder"} />
-					<TextField
-						label={"Label"}
-						description={"This is a description of the field."}
-					/>
-					<TextField label={"Label"} value={"Hello, world!"} isReadOnly />
-					<TextField label={"Label"} errorMessage={"This is an error message."} />
-					<TextField
-						label={"Label"}
-						placeholder={"Placeholder"}
-						description={"This is a description of the field."}
-						errorMessage={"This is an error message."}
-					/>
-					<TextField
-						label={"Disabled"}
-						placeholder={"Placeholder"}
-						description={"This is a description of the field."}
-						errorMessage={"This is an error message."}
-						isDisabled
-					/>
+			<Card title={"Text Field and Simple Text Field"} icon={"title"} subtitle={"Form"}>
+				<div
+					class={style.horizontal}
+					style={{ margin: "10px", "align-items": "self-start" }}
+				>
+					<div class={style.vertical} style={{ flex: "1" }}>
+						<TextField label={"Label"} />
+						<TextField label={"Label"} placeholder={"Placeholder"} />
+						<TextField label={"Label"} icon={"search"} placeholder={"With an icon"} />
+						<TextField
+							label={"Label"}
+							description={"This is a description of the field."}
+						/>
+						<TextField
+							label={"Label"}
+							placeholder={"With an action"}
+							action={<TriggerButton icon={"visibility_off"} label={"Delete"} />}
+						/>
+						<TextField label={"Label"} value={"Hello, world!"} isReadOnly />
+						<TextField label={"Label"} errorMessage={"This is an error message."} />
+						<TextField
+							label={"Label"}
+							placeholder={"Placeholder"}
+							description={"This is a description of the field."}
+							errorMessage={"This is an error message."}
+						/>
+						<TextField
+							label={"Disabled"}
+							placeholder={"Placeholder"}
+							description={"This is a description of the field."}
+							errorMessage={"This is an error message."}
+							isDisabled
+						/>
+					</div>
+					<div class={style.vertical} style={{ flex: "1" }}>
+						<Callout title={"Usage"} intent={CalloutIntent.Warning}>
+							The <strong>Simple Text Field</strong> is dedicated to{" "}
+							<strong>HeaderBar</strong>, which does not have that much space for
+							large inputs.
+						</Callout>
+						<SimpleTextField label={"Simple Label"} />
+						<SimpleTextField label={"Simple Label"} placeholder={"Placeholder"} />
+						<SimpleTextField
+							label={"Simple Label"}
+							icon={"filter_alt"}
+							action={<TriggerButton icon={"expand_more"} label={"Menu"} />}
+						/>
+						<SimpleTextField
+							label={"Simple Label"}
+							placeholder={"We have a problem"}
+							errorMessage={"This is wrong"}
+						/>
+						<SimpleTextField
+							label={"Simple Label"}
+							placeholder={"Disabled"}
+							isDisabled
+						/>
+						<HeaderBar
+							actions={[
+								<TriggerButtonGroup>
+									<TriggerButton icon={"format_align_left"} label={"Left"} />
+									<TriggerButton icon={"format_align_center"} label={"Middle"} />
+									<TriggerButton icon={"format_align_right"} label={"Right"} />
+								</TriggerButtonGroup>,
+								<HeaderDivider />,
+								<SimpleTextField
+									label={"Simple Label"}
+									icon={"filter_alt"}
+									action={<TriggerButton icon={"expand_more"} label={"Menu"} />}
+									style={{ width: "180px" }}
+								/>,
+							]}
+						/>
+					</div>
 				</div>
 			</Card>
 		</div>
--- packages/widgets/src/components/TextField.tsx
+++ packages/widgets/src/components/TextField.tsx
@@ -1,11 +1,13 @@
 import type { AriaTextFieldProps } from "@solid-aria/primitives";
-import { createFocusWithin, createTextField } from "@solid-aria/primitives";
-import { createSignal, JSX, Show } from "solid-js";
+import { createFocus, createTextField } from "@solid-aria/primitives";
+import { createSignal, JSX, mergeProps, Show } from "solid-js";
 
 import style from "../styles/Fields.module.scss";
 
 interface TextFieldProps extends AriaTextFieldProps<"input"> {
 	style?: JSX.CSSProperties;
+	action?: JSX.Element;
+	icon?: string;
 }
 
 export const TextField = (props: TextFieldProps) => {
@@ -14,18 +16,14 @@ export const TextField = (props: TextFieldProps) => {
 		props,
 		() => ref,
 	);
-	const [isFocusWithin, onFocusWithinChange] = createSignal(false);
-	const { focusWithinProps } = createFocusWithin({ onFocusWithinChange });
+	const [isFocus, onFocusChange] = createSignal(false);
+	const { focusProps } = createFocus({ onFocusChange });
 
 	return (
 		<div
-			{...focusWithinProps}
 			class={style.group}
 			classList={{
 				[style.disabled]: !!props.isDisabled,
-				[style.error]: !!props.errorMessage,
-				[style.readOnly]: props.isReadOnly,
-				[style.focus]: isFocusWithin(),
 			}}
 			style={props.style}
 		>
@@ -38,7 +36,22 @@ export const TextField = (props: TextFieldProps) => {
 			<label class={style.label} {...labelProps}>
 				{props.label}
 			</label>
-			<input class={style.input} {...inputProps} ref={ref} />
+			<div
+				class={style.inputAction}
+				classList={{
+					[style.focus]: isFocus(),
+					[style.error]: !!props.errorMessage,
+					[style.readOnly]: props.isReadOnly,
+				}}
+			>
+				<Show when={!!props.icon}>
+					<span class={`${style.icon} material-symbols-outlined`}>{props.icon}</span>
+				</Show>
+				<input class={style.input} {...mergeProps(inputProps, focusProps)} ref={ref} />
+				<Show when={!!props.action}>
+					<div class={style.action}>{props.action}</div>
+				</Show>
+			</div>
 			<Show when={props.errorMessage && !props.isDisabled} keyed>
 				<div {...errorMessageProps} class={style.errorMessage}>
 					{props.errorMessage}
@@ -48,6 +61,38 @@ export const TextField = (props: TextFieldProps) => {
 				<div {...descriptionProps} class={style.description}>
 					{props.description}
 				</div>
+			</Show>
+		</div>
+	);
+};
+
+type SimpleTextFieldProps = Omit<TextFieldProps, "isRequired" | "isReadOnly" | "description">;
+
+export const SimpleTextField = (props: SimpleTextFieldProps) => {
+	let ref!: HTMLInputElement;
+	const { labelProps, inputProps } = createTextField(props, () => ref);
+	const [isFocus, onFocusChange] = createSignal(false);
+	const { focusProps } = createFocus({ onFocusChange });
+
+	return (
+		<div
+			class={`${style.inputAction} ${style.simple}`}
+			classList={{
+				[style.disabled]: !!props.isDisabled,
+				[style.error]: !!props.errorMessage,
+				[style.focus]: isFocus(),
+			}}
+			style={props.style}
+		>
+			<label {...labelProps} class={style.hiddenLabel}>
+				{props.label}
+			</label>
+			<Show when={!!props.icon}>
+				<span class={`${style.icon} material-symbols-outlined`}>{props.icon}</span>
+			</Show>
+			<input class={style.input} {...mergeProps(inputProps, focusProps)} ref={ref} />
+			<Show when={!!props.action}>
+				<div class={style.action}>{props.action}</div>
 			</Show>
 		</div>
 	);
