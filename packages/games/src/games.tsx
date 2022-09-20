e4aa2dfe6064bf1d13f98cfca51e0207092e84be
Cédric Eberhardt <hello+code@cedeber.fr>
2022-09-21 01:10:32 +0200

    remove unsafe header

--- packages/games/src/games.tsx
+++ packages/games/src/games.tsx
@@ -7,9 +7,7 @@ import style from "./games.module.scss";
 
 const { hostname } = window.location;
 const client = new GraphQLClient(`//${hostname}:${import.meta.env.VITE_PORT}/graphql`, {
-	headers: {
-		Host: "premio.dev.cedeber.fr",
-	},
+	// headers: {},
 });
 const sdk = getSdk(client);
 
