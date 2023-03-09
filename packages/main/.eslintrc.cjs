6d582391a4dffaf7c468b7e2edc2318fd39cecf5
Cédric Eberhardt <hello+code@cedeber.fr>
2023-03-09 19:24:06 +0100

    Fix eslint

--- packages/main/.eslintrc.cjs
+++ packages/main/.eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
 	],
 	overrides: [
 		{
-			files: ["/cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"],
+			files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"],
 			extends: ["plugin:cypress/recommended"],
 		},
 	],
