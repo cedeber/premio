cd9d4c5932b6959996821ecac200e32dbad9f7ed
Cédric Eberhardt <hello+code@cedeber.fr>
2023-03-10 13:19:54 +0100

    play with pinia

---
+++ packages/main/src/components/CounterButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useCounterStore } from "@/stores/counter";
+import { storeToRefs } from "pinia";
+
+const store = useCounterStore();
+const { count, doubleCount } = storeToRefs(store);
+const { increment } = store;
+</script>
+
+<template>
+	<button @click="increment">{{ count }}</button>
+	{{ doubleCount }}
+</template>
+
+<style scoped></style>
--- packages/main/src/views/AboutView.vue
+++ packages/main/src/views/AboutView.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import CounterButton from "@/components/CounterButton.vue";
+</script>
+
 <template>
 	<div class="about">
 		<h1>This is an about page</h1>
+		<CounterButton />
 	</div>
 </template>
 
@@ -11,5 +16,13 @@
 		display: flex;
 		align-items: center;
 	}
+}
+</style>
+
+<style scoped>
+.about {
+	flex-direction: column;
+	gap: 10px;
+	justify-content: center;
 }
 </style>
